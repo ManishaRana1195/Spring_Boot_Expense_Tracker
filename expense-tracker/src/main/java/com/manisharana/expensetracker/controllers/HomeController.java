ad58680f537f3c6a10f00ef6a263b20f15c7d88c
ManishaRana1195 <manisha1195@gmail.com>
2020-05-06 16:59:32 -0500

    added screenshot

---
+++ expense-tracker/src/main/java/com/manisharana/expensetracker/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package com.manisharana.expensetracker.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+    
+}
